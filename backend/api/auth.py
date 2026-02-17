d4ef8f741f8a2caa2225c425c90271a9d8496317
Ailfriduwan <6510110565@email.psu.ac.th>
2026-02-17 19:21:44 +0700

    fix test api

--- backend/api/auth.py
+++ backend/api/auth.py
@@ -63,8 +63,7 @@ def list_users():
 
 @bp.get("/test-email")
 def test_email():
-    from utils.email import send_reset_email
-
+    from backend.utils.email_service import send_reset_email
 
     send_reset_email(
         to_email="abdulfeera2@gmail.com",
