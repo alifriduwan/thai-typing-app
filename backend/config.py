06aaa50f32137ef2383ce85db0d9003b1e496ad7
Ailfriduwan <6510110565@email.psu.ac.th>
2026-02-20 15:38:40 +0700

    fix token expired

--- backend/config.py
+++ backend/config.py
@@ -1,8 +1,11 @@
 import os
+from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "dev-jwt")
+
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=7)
 
     db_url = os.getenv("DATABASE_URL")
 
